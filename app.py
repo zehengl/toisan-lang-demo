a3da87e2dc78189f2e0ad17f9a0ec35b193f3b33
Zeheng Li <imzehengl@gmail.com>
2020-03-07 22:34:41 -0700

    some error handling

--- app.py
+++ app.py
@@ -15,12 +15,25 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "SECRET_KEY")
 app.config["BOOTSTRAP_SERVE_LOCAL"] = True
 
 
+def transpile(program):
+    try:
+        code, _ = parse(program)
+    except:
+        code = None
+
+    return code
+
+
 @app.route("/", methods=["get", "post"])
 def index():
     form = ProgramForm(request.form)
 
-    python_code, _ = parse(form.program.data)
-    num_lines = len(python_code.split("\n"))
+    python_code = transpile(form.program.data)
+
+    if python_code:
+        num_lines = len(python_code.split("\n"))
+    else:
+        num_lines = 0
 
     return render_template(
         "index.html", form=form, python_code=python_code, num_lines=num_lines
