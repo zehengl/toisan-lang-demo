a8cddfb774c496d3bb080725b32bf96af870b45f
Zeheng Li <imzehengl@gmail.com>
2021-11-09 11:14:01 -0700

    set default

--- forms.py
+++ forms.py
@@ -3,5 +3,9 @@ from wtforms import SubmitField, TextAreaField
 
 
 class ProgramForm(FlaskForm):
-    program = TextAreaField("台山话program", render_kw={"rows": 10},)
+    program = TextAreaField(
+        "台山话program",
+        default="",
+        render_kw={"rows": 10},
+    )
     submit = SubmitField("Transpile")
